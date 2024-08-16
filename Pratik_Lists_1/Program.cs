e20e2194b5e7e96361a3ad83320d58906980f531
Hasan İncel <h_incel@hotmail.com>
2024-08-17 01:35:37 +0300

    Add project files.

---
+++ Pratik_Lists_1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<string> kisiler = new List<string>();
+
+kisiler.Add("Bülent Ersoy");
+kisiler.Add("Ajda Pekkan");
+kisiler.Add("Ebru Gündeş");
+kisiler.Add("Hadise");
+kisiler.Add("Hande Yener");
+kisiler.Add("Tarkan");
+kisiler.Add("Funda Arar");
+kisiler.Add("Demet Akalın");
+
+foreach (string kisi in kisiler)
+{
+    Console.WriteLine(kisi);
+}
